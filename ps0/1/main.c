3725155a0e782042adafb01fdc36054d25f60d38
Sean Walsh <sdwalsh@mirango.io>
2019-08-05 21:09:29 -0400

    ps0 1, 2

---
+++ ps0/1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    unsigned long length = 0;
+    while (fgetc(stdin) != EOF) {
+        length++;
+    }
+    fprintf(stdout, "%lu\n", length);
+    return 1;
+}
---
+++ ps0/2/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    unsigned long number_of_bytes = 0;
+    unsigned int number_of_lines = 0;
+    unsigned int number_of_words = 0;
+    unsigned int current_word_length = 0;
+
+    while (1) {
+        int c = fgetc(stdin);
+        if (c == EOF) {
+            break;
+        }
+        number_of_bytes++;
+        if (isspace(c)) {
+            if (c == 10) {
+                number_of_lines++;
+            }
+            if (current_word_length > 0) {
+                number_of_words++;
+                current_word_length = 0;
+            }
+        } else {
+            current_word_length++;
+        }
+    }
+
+    if (current_word_length > 0) {
+        number_of_words++;
+    }
+
+    fprintf(stdout, "%u %u %lu\n", number_of_lines, number_of_words, number_of_bytes);
+    return 1;
+}
